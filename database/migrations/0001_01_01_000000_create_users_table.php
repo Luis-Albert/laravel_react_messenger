b005d022a3f1ab4cdc4cdb2fd52ec55472b5b5c9
Luis Alberto Huaman <56800427+Luis-Albert@users.noreply.github.com>
2025-05-10 17:52:13 -0500

    Updated migratios Users
    se añadio a la tabla users el campo enum

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('avatar')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'user'])->default('user');
             $table->rememberToken();
             $table->boolean('is_admin')->default(false);
             $table->timestamp('blocked_at')->nullable();

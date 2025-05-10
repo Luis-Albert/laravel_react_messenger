d92a704e771a2eac14a1719cf2a8c552bab93d03
Luis Alberto Huaman <56800427+Luis-Albert@users.noreply.github.com>
2025-05-10 17:53:40 -0500

    Updated DatabaseSeeder
    Momentaneamente anulamos los codigos previamente creados y se hizo un nuevo llamado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,42 +19,47 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Luis Alberto',
-            'email' => 'luis@example.com',
-            'password' => bcrypt('password'),
-            'is_admin' => true,
-        ]);
-        User::factory()->create([
-            'name' => 'Jose Luis',
-            'email' => 'jose@example.com',
-            'password' => bcrypt('password'),
+        $this->call([
+            UserSeeder::class,
+            ContactSeeder::class,
         ]);
 
-        User::factory(10)->create();
-        for ($i = 0; $i < 5; $i++) {
-            $group = Group::factory()->create([
-                'owner_id' => 1,
-            ]);
+        // User::factory()->create([
+        //     'name' => 'Luis Alberto',
+        //     'email' => 'luis@example.com',
+        //     'password' => bcrypt('password'),
+        //     'is_admin' => true,
+        // ]);
+        // User::factory()->create([
+        //     'name' => 'Jose Luis',
+        //     'email' => 'jose@example.com',
+        //     'password' => bcrypt('password'),
+        // ]);
 
-            $users = User::inRandomOrder()->limit(rand(2, 5))->pluck('id');
-            $group->users()->attach(array_unique([1, ...$users]));
-        }
+        // User::factory(10)->create();
+        // for ($i = 0; $i < 5; $i++) {
+        //     $group = Group::factory()->create([
+        //         'owner_id' => 1,
+        //     ]);
 
-        Message::factory(1000)->create();
-        $messages = Message::whereNull('group_id')->orderBy('created_at')->get();
-        $conversations = $messages->groupBy(function ($message) {
-            return collect([$message->sender_id, $message->receiver_id])->sort()->implode('_');
-        })->map(function ($groupMessages) {
-            return [
-                'user_id1' => $groupMessages->first()->sender_id,
-                'user_id2' => $groupMessages->first()->receiver_id,
-                'last_message_id' => $groupMessages->last()->id,
-                'created_at' => new Carbon(),
-                'updated_at' => new Carbon(),
-            ];
-        })->values();
+        //     $users = User::inRandomOrder()->limit(rand(2, 5))->pluck('id');
+        //     $group->users()->attach(array_unique([1, ...$users]));
+        // }
 
-        Conversation::insertOrIgnore($conversations->toArray());
+        // Message::factory(1000)->create();
+        // $messages = Message::whereNull('group_id')->orderBy('created_at')->get();
+        // $conversations = $messages->groupBy(function ($message) {
+        //     return collect([$message->sender_id, $message->receiver_id])->sort()->implode('_');
+        // })->map(function ($groupMessages) {
+        //     return [
+        //         'user_id1' => $groupMessages->first()->sender_id,
+        //         'user_id2' => $groupMessages->first()->receiver_id,
+        //         'last_message_id' => $groupMessages->last()->id,
+        //         'created_at' => new Carbon(),
+        //         'updated_at' => new Carbon(),
+        //     ];
+        // })->values();
+
+        // Conversation::insertOrIgnore($conversations->toArray());
     }
 }
